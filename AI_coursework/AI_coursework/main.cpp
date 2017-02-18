a0ee125e7f13d3605b5ecd26953ec34ec22b5832
kath-bruce <kath-bruce@hotmail.com>
2017-02-18 14:08:19 +0000

    updated graphdata with positions

--- AI_coursework/AI_coursework/main.cpp
+++ AI_coursework/AI_coursework/main.cpp
@@ -2,7 +2,31 @@
 
 int main(int argc, char *argv[]) {
 	
+	/*
+	//nodes will be stored in vector of positions - Position.cpp and header needed
+	//came_from = vector/unordered_set/set of nodes(ints)
+	//cost_so_far = vector/unordered_set/set of 
 
+	frontier = PriorityQueue()
+	frontier.put(start, 0)
+	came_from = {}
+	cost_so_far = {}
+	came_from[start] = None
+	cost_so_far[start] = 0
 
+	while not frontier.empty():
+		current = frontier.get()
+
+		if current == goal:
+			break
+
+		for next in graph.neighbors(current):
+			new_cost = cost_so_far[current] + graph.cost(current, next)
+			if next not in cost_so_far or new_cost < cost_so_far[next]:
+				cost_so_far[next] = new_cost
+				priority = new_cost + heuristic(goal, next)
+				frontier.put(next, priority)
+				came_from[next] = current
+	*/
 	return 0;
 }
