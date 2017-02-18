03c46e009bb9cfb6b7ab567e58e932ebd9966f33
kath-bruce <kath-bruce@hotmail.com>
2017-02-18 13:33:49 +0000

    graph class header and cpp + graph data.txt

---
+++ AI_coursework/AI_coursework/Graph.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef GRAPH_H
+#define GRAPH_H
+
+class Graph {
+public:
+	Graph();
+	//start node
+	//num of nodes
+	//unordered map of edges + cost
+	//read from file in constructor
+	//in main - priority queue 
+	//goal node
+	//neighbours - data structure? vector?
+private:
+	
+};
+
+#endif
